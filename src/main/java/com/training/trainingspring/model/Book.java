4befd30dd7fb3d4597326840e49bc8f2d8c49118
Franco B <franco.barravecchia2002@gmail.com>
2023-08-04 21:54:54 -0300

    update: book variables

--- src/main/java/com/training/trainingspring/model/Book.java
+++ src/main/java/com/training/trainingspring/model/Book.java
@@ -3,6 +3,7 @@ package com.training.trainingspring.model;
 import lombok.*;
 
 import java.time.Year;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -14,5 +15,6 @@ public class Book {
     private UUID id;
     private String title;
     private Year year;
-    private UUID genre;
+    private UUID author;
+    private List<UUID> genre;
 }
